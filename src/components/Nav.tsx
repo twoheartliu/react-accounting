b3015b7869b14efcfdb2ea9d036efb8d3ab2c6a5
twoheart <twoheart@vip.qq.com>
2020-07-13 17:26:34 +0800

    封装 svg 以及引用 svg 文件夹

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import Icon from './Icon';
+
+
 
 const NavWrapper = styled.nav`
 box-shadow: 0 0 3px rgba(0, 0, 0,  0.25) ;
@@ -10,7 +13,15 @@ box-shadow: 0 0 3px rgba(0, 0, 0,  0.25) ;
     > li {
       width: 33.33%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -19,12 +30,15 @@ const Nav = () => {
   return <NavWrapper>
     <ul>
       <li>
+        <Icon name={'tags'}/>
         <Link to="/tags">标签页</Link>
       </li>
       <li>
+        <Icon name={'money'}/>
         <Link to="/money">记账页</Link>
       </li>
       <li>
+        <Icon name={'statistics'}/>
         <Link to="/statistics">统计页</Link>
       </li>
     </ul>
