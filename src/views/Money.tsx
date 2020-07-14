0f79534043f1eeddb65deb7e97a3687a28f610ef
twoheart <twoheart@vip.qq.com>
2020-07-14 18:35:12 +0800

    Money 模块的 NumberPad UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -70,7 +70,64 @@ const CategorySection = styled.section`
   }
 `;
 const NumberPadSection = styled.section`
-
+  display: flex;
+  flex-direction: column;
+  > .output {
+    background:#fff;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0px -5px 5px -5px rgba(0, 0, 0, 0.25),
+                inset 0px 5px 5px -5px rgba(0, 0, 0, 0.25);    
+  }
+  > .pad {
+    &.clearfix::after {
+      content: '';
+      display: block;
+      clear: both;
+    }
+    > button {
+      border: none;
+      float:left;
+      width: 25%;
+      height: 64px;
+      font-size: 18px;
+      &.ok {
+        height: calc(64 * 2px);
+        float: right;
+      }
+      &.zero {
+        width: calc(25% * 2);
+      }
+      &:nth-child(1) {
+        background: red;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: green;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: orange;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: blue;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: yellowgreen;
+      }
+      &:nth-child(12),
+      &:nth-child(14) {
+        background: lightcoral;
+      }
+    }
+  }
 `;
 
 
@@ -99,8 +156,8 @@ const Money = () => {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className="output">100</div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -112,8 +169,8 @@ const Money = () => {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
           <button>.</button>
         </div>
       </NumberPadSection>
