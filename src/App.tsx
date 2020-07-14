edd23f79f03a7e9936a38fa088f15a65139917a2
twoheart <twoheart@vip.qq.com>
2020-07-14 15:27:31 +0800

    Money 模块的 TagsSection UI

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,33 @@ import Money from './views/Money';
 import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NotFound from './views/NotFound';
+import styled from 'styled-components';
 
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/tags">
-          <Tags/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics />
-        </Route>
-        <Redirect exact from='/' to='/money'/>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from='/' to='/money'/>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
