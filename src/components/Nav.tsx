ba25e62b10955949c08ec58e818e1677fc8c2c74
twoheart <twoheart@vip.qq.com>
2020-07-14 15:54:05 +0800

    Money 模块的 NotesSection UI

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,8 @@ import Icon from './Icon';
 
 
 const NavWrapper = styled.nav`
-box-shadow: 0 0 3px rgba(0, 0, 0,  0.25) ;
+  background: #fff;
+  box-shadow: 0 0 3px rgba(0, 0, 0,  0.25) ;
   line-height: 24px;
   > ul {  
     display: flex;
