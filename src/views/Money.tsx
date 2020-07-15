c3c12bfad574c7cc1eb0d49a8c6043fb7cae7c22
twoheart <twoheart@vip.qq.com>
2020-07-15 10:24:21 +0800

    Money 模块的 NumberPad UI 配色

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -101,30 +101,32 @@ const NumberPadSection = styled.section`
         width: calc(25% * 2);
       }
       &:nth-child(1) {
-        background: red;
+        background: #F2F2F2;
       }
       &:nth-child(2),
       &:nth-child(5) {
-        background: green;
+        background: #E0E0E0;
       }
       &:nth-child(3),
       &:nth-child(6),
       &:nth-child(9) {
-        background: orange;
+        background: #D3D3D3;
       }
       &:nth-child(4),
       &:nth-child(7),
       &:nth-child(10) {
-        background: blue;
+        background: #C1C1C1;
       }
       &:nth-child(8),
       &:nth-child(11),
       &:nth-child(13) {
-        background: yellowgreen;
+        background: #B8B8B8;
       }
-      &:nth-child(12),
+      &:nth-child(12) {
+        background: #9A9A9A;
+      }
       &:nth-child(14) {
-        background: lightcoral;
+        background: #A9A9A9;
       }
     }
   }
