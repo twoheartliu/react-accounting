2166971263635e6404a56d72e9e484a5cc049caf
twoheart <twoheart@vip.qq.com>
2020-07-17 15:35:09 +0800

    Money 模块，NumberPad 拆分封装

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,68 +1,6 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
-
-const Wrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  > .output {
-    background:#fff;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0px -5px 5px -5px rgba(0, 0, 0, 0.25),
-                inset 0px 5px 5px -5px rgba(0, 0, 0, 0.25);    
-  }
-  > .pad {
-    &.clearfix::after {
-      content: '';
-      display: block;
-      clear: both;
-    }
-    > button {
-      border: none;
-      float:left;
-      width: 25%;
-      height: 64px;
-      font-size: 18px;
-      &.ok {
-        height: calc(64 * 2px);
-        float: right;
-      }
-      &.zero {
-        width: calc(25% * 2);
-      }
-      &:nth-child(1) {
-        background: #F2F2F2;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: #E0E0E0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: #D3D3D3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: #C1C1C1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: #B8B8B8;
-      }
-      &:nth-child(12) {
-        background: #9A9A9A;
-      }
-      &:nth-child(14) {
-        background: #A9A9A9;
-      }
-    }
-  }
-`;
+import {Wrapper} from './NumberPad/Wrapper';
+import {generateOutput} from './NumberPad/generateOutput';
 
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = useState('0');
@@ -77,41 +15,11 @@ const NumberPadSection: React.FC = () => {
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) return;
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '' || (output[0] === '0' && !output.includes('.')) ) {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case '.':
-        if (output.includes('.')) return;
-        setOutput(output + text);
-        break;
-      case '删除':
-        if (output.length <= 1) {
-          setOutput('');
-        } else if (output.length > 1) {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case '清空':
-        setOutput('');
-        break;
-      case 'OK':
-        console.log('OK');
-        break;
+    if (text === 'OK') {
+      // wip
+      return;
     }
+    setOutput(generateOutput(text, output));
   };
   return <Wrapper>
     <div className="output">{output}</div>
