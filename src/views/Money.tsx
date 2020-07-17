0992f25e3dcc8db3880012bf281845cfb1c5a1cb
twoheart <twoheart@vip.qq.com>
2020-07-17 11:29:55 +0800

    Money 模块，NumberPad WIP

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import styled from 'styled-components';
+import {TagsSection} from './Money/TagsSection';
 import {NoteSection} from './Money/NoteSection';
 import {CategorySection} from './Money/CategorySection';
 import {NumberPadSection} from './Money/NumberPadSection';
-import {TagsSection} from './Money/TagsSection';
 
 
 const MyLayout = styled(Layout)`
--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, {useState} from 'react';
 
 const Wrapper = styled.section`
   display: flex;
@@ -64,10 +64,46 @@ const Wrapper = styled.section`
   }
 `;
 
-const NumberPadSection:React.FC = () => {
+const NumberPadSection: React.FC = () => {
+  const [output, setOutput] = useState('0');
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent;
+    if (text === null) return;
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        if (output === '0') {
+          setOutput(text);
+        } else {
+          setOutput(output + text)
+        }
+        console.log(text);
+        break;
+      case '删除':
+        console.log('删除');
+        break;
+      case '清空':
+        console.log('清空');
+        break;
+      case 'OK':
+        console.log('OK');
+        break;
+      case '.':
+        console.log('点');
+        break;
+    }
+  };
   return <Wrapper>
-    <div className="output">100</div>
-    <div className="pad clearfix">
+    <div className="output">{output}</div>
+    <div className="pad clearfix" onClick={onClickButtonWrapper}>
       <button>1</button>
       <button>2</button>
       <button>3</button>
