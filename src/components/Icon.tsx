8768da5f029650516cf6a6939670d848229d7404
twoheart <twoheart@vip.qq.com>
2020-07-23 10:55:06 +0800

    点击回退

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import classNames from 'classnames';
 
-// require('icons/money.svg');
-// require('icons/tags.svg');
-// require('icons/statistics.svg');
 const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 type Props = {
   name?: string;
-}
+} & React.SVGAttributes<SVGElement>
 
 const Icon = (props: Props) => {
-  return <svg className="icon">
+  const {name, children, className, ...rest} = props;
+  return (<svg className={classNames('icon', className)} {...rest}>
     {props.name && <use xlinkHref={'#' + props.name}/>}
-  </svg>;
+  </svg>);
 };
 
 export default Icon;
--- src/useTags.tsx
+++ src/useTags.tsx
@@ -21,18 +21,11 @@ const useTags = () => {
     }
     return result;
   };
-  const updateTag = (id: number, obj:{name: string}) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    // 把 tagsClone 的 第 index 个删掉，换成 {id: id, name: obj.name}
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    setTags(tagsClone);
+  const updateTag = (id: number, {name}:{name: string}) => {
+    setTags(tags.map(tag => tag.id === id ? {id, name} : tag));
   }
   const deleteTag = (id: number) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    setTags(tags.filter(tag => tag.id !== id));
   }
   return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
 };
